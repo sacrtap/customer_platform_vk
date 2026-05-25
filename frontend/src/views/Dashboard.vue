c9403a659073c8fb73acb3b000324a9871d773f7
sacrtap <89307033@qq.com>
2026-05-26 01:06:29 +0800

    style(frontend): remove search and settings buttons from header

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -497,24 +497,6 @@
         </div>
 
         <div class="header-right">
-          <!-- 搜索 -->
-          <ActionButton label="搜索" @click="$message.info('搜索功能开发中')">
-            <svg
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </ActionButton>
-
           <!-- 通知 -->
           <ActionButton label="消息通知" :badge="3" @click="$message.info('通知功能开发中')">
             <svg
@@ -529,24 +511,6 @@
                 stroke-linejoin="round"
                 stroke-width="2"
                 d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-          </ActionButton>
-
-          <!-- 设置 -->
-          <ActionButton label="系统设置" @click="$message.info('设置功能开发中')">
-            <svg
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
               />
             </svg>
           </ActionButton>
